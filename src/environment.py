03a78f68092296406439e3479cb564279bc20947
Andy Craze <accraze@gmail.com>
2019-04-12 12:37:18 -0700

    move transitions model to separate module

--- src/environment.py
+++ src/environment.py
@@ -13,92 +13,12 @@ class Environment:
 
     actions = [NORTH, SOUTH, EAST, WEST, PUT]
 
-    def __init__(self, debug=False):
+    def __init__(self, transitions, debug=False):
         self.states_space = self._build_state_space()
         self.actions_space = self._build_actions_space()
         self.rewards_space = self._build_rewards_space()
         self.debug = debug
-
-    def test_boundary(self, player, action):
-        if (player + action > FIELD_MAX or player + action < FIELD_MIN):
-            return 0
-        return action
-
-    def transition(self, state, actions):
-        ball, player1, player2 = self.states_space[state]
-        self._log('trans: actions: {}'.format(actions))
-        actions[0] = self.test_boundary(player1, actions[0])
-        actions[1] = self.test_boundary(player2, actions[1])
-        self._log('trans: curent_state {} {} {}'.format(ball, player1, player2))
-        next_state = self._get_next_state(player1, player2, actions, ball)
-        self._log('trans: next_state {}'.format(next_state))
-        next_state_index = lookup_vector_index(self.states_space, next_state, state=state)
-        return next_state_index
-
-    def _check_collision(self, player_coords):
-        return player_coords[0] == player_coords[1]
-
-    def _get_next_state(self, p1, p2, actions, ball):
-        player_steps = ((p1 + actions[0]), (p2 + actions[1]))
-        if self._check_collision(player_steps):
-            self._log('collision! p1: {} p2: {}'.format(player_steps[0], player_steps[1]))
-            next_state = self._handle_collision(p1, p2, ball, actions)
-        else:
-            next_state = self._move_to_ball(p1, ball, p2, actions)
-        return next_state
-
-    def _handle_collision(self, p1, p2, ball, actions):
-        coin_flip = np.random.randint(2)
-        if self._p1_moves_first(coin_flip):
-            if self._p1_has_ball(ball):
-                next_state = self._p1_move_ball(p1, ball, p2, actions[0])
-            else:
-                next_state = self._p1_gets_ball(p1, ball, p2, actions[0])
-        else:
-            if not self._p1_has_ball(ball):
-                next_state = self._p2_move_ball(p1, ball, p2, actions[1])
-            else:
-                next_state = self._p2_gets_ball(p1, ball, p2, actions[1])
-
-        return next_state
-
-    def _p1_moves_first(self, coin_flip):
-        return coin_flip == 0
-
-    def _p1_has_ball(self, ball):
-        return ball == 0
-
-    def _p1_move_ball(self, p1, ball, p2, action):
-        p1 = p1 + action
-        return self._new_state(ball, p1, p2)
-
-    def _p1_gets_ball(self, p1_cell, ball, p2_cell, action):
-        """
-        P1 moves and gets ball.
-        """
-        ball = ball - 1  # flip ball
-        p1_cell = p1_cell + action
-        return self._new_state(ball, p1_cell, p2_cell)
-
-    def _p2_move_ball(self, p1, ball, p2, action):
-        p2 = p2 + action
-        return self._new_state(ball, p1, p2)
-
-    def _p2_gets_ball(self, p1_cell, ball, p2_cell, action):
-        """
-        P2 moves and gets ball.
-        """
-        ball = ball + 1  # flip ball
-        p2_cell = p2_cell + action
-        return self._new_state(ball, p1_cell, p2_cell)
-
-    def _move_to_ball(self, p1_cell, ball, p2_cell, actions):
-        p1_cell = p1_cell + actions[0]
-        p2_cell = p2_cell + actions[1]
-        return self._new_state(ball, p1_cell, p2_cell)
-
-    def _new_state(self, ball, p1_cell, p2_cell):
-        return [ball, p1_cell, p2_cell]
+        self.transition_model = transitions
 
     def _build_state_space(self):
         state_space = np.array([[0, 0, h] for h in range(1, 8)] +
@@ -108,9 +28,10 @@ class Environment:
                                 for l in range(0, 8) if k != l])
         return state_space
 
-    def _log(self, msg):
-        if self.debug:
-            print(msg)
+    def transition(self, state, actions):
+        next_state = self.transition_model.get_next_state(
+            self.states_space[state], state, self.states_space, actions)
+        return next_state
 
     def _build_actions_space(self):
         actions_space = list(permutations(self.actions, 2))
---
+++ src/transitions.py
@@ -0,0 +1,99 @@
+import numpy as np
+
+from src.settings import FIELD_MAX, FIELD_MIN
+from src.utils import lookup_vector_index
+
+
+class Transitions:
+
+    def __init__(self, debug=False):
+        self.debug = debug
+
+    def get_next_state(self, state, state_idx, states_space, actions):
+        ball, player1, player2 = state
+        self._log('trans: actions: {}'.format(actions))
+        actions[0] = self.test_boundary(player1, actions[0])
+        actions[1] = self.test_boundary(player2, actions[1])
+        self._log('trans: curent_state {} {} {}'.format(
+            ball, player1, player2))
+        next_state = self._get_next_state(player1, player2, actions, ball)
+        self._log('trans: next_state {}'.format(next_state))
+        next_state_index = lookup_vector_index(
+            states_space, next_state, state=state_idx)
+        return next_state_index
+
+    def test_boundary(self, player, action):
+        if (player + action > FIELD_MAX or player + action < FIELD_MIN):
+            return 0
+        return action
+
+    def _log(self, msg):
+        if self.debug:
+            print(msg)
+
+    def _check_collision(self, player_coords):
+        """Check if both player coords are in same cell. Returns bool."""
+        return player_coords[0] == player_coords[1]
+
+    def _get_next_state(self, p1, p2, actions, ball):
+        player_steps = ((p1 + actions[0]), (p2 + actions[1]))
+        if self._check_collision(player_steps):
+            self._log('collision! p1: {} p2: {}'.format(
+                player_steps[0], player_steps[1]))
+            next_state = self._handle_collision(p1, p2, ball, actions)
+        else:
+            next_state = self._advance(p1, ball, p2, actions)
+        return next_state
+
+    def _handle_collision(self, p1, p2, ball, actions):
+        coin_flip = np.random.randint(2)
+        if self._p1_moves_first(coin_flip):
+            if self._p1_has_ball(ball):
+                next_state = self._p1_move_ball(p1, ball, p2, actions[0])
+            else:
+                next_state = self._p1_gets_ball(p1, ball, p2, actions[0])
+        else:
+            if not self._p1_has_ball(ball):
+                next_state = self._p2_move_ball(p1, ball, p2, actions[1])
+            else:
+                next_state = self._p2_gets_ball(p1, ball, p2, actions[1])
+
+        return next_state
+
+    def _p1_moves_first(self, coin_flip):
+        return coin_flip == 0
+
+    def _p1_has_ball(self, ball):
+        return ball == 0
+
+    def _p1_move_ball(self, p1, ball, p2, action):
+        p1 = p1 + action
+        return self._new_state(ball, p1, p2)
+
+    def _p1_gets_ball(self, p1_cell, ball, p2_cell, action):
+        """
+        P1 moves and gets ball.
+        """
+        ball = ball - 1  # flip ball
+        p1_cell = p1_cell + action
+        return self._new_state(ball, p1_cell, p2_cell)
+
+    def _p2_move_ball(self, p1, ball, p2, action):
+        p2 = p2 + action
+        return self._new_state(ball, p1, p2)
+
+    def _p2_gets_ball(self, p1_cell, ball, p2_cell, action):
+        """
+        P2 moves and gets ball.
+        """
+        ball = ball + 1  # flip ball
+        p2_cell = p2_cell + action
+        return self._new_state(ball, p1_cell, p2_cell)
+
+    def _advance(self, p1_cell, ball, p2_cell, actions):
+        p1_cell = p1_cell + actions[0]
+        p2_cell = p2_cell + actions[1]
+        return self._new_state(ball, p1_cell, p2_cell)
+
+    def _new_state(self, ball, p1_cell, p2_cell):
+        return [ball, p1_cell, p2_cell]
