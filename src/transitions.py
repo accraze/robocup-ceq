7fa20be8b65d45742f2f39f947e466f349135ab0
Andy Craze <accraze@gmail.com>
2019-04-14 16:38:08 -0700

    add transitions docstrings

--- src/transitions.py
+++ src/transitions.py
@@ -6,6 +6,11 @@ from src.utils import lookup_vector_index
 
 class Transitions:
 
+    """
+    Handles all movements for grid soccer.
+    Also checks out-of-bounds and collisions.
+    """
+
     def __init__(self, debug=False):
         self.debug = debug
 
@@ -23,6 +28,7 @@ class Transitions:
         return next_state_index
 
     def test_boundary(self, player, action):
+        """Check out of bounds errors."""
         if (player + action > FIELD_MAX or player + action < FIELD_MIN):
             return 0
         return action
@@ -36,6 +42,7 @@ class Transitions:
         return player_coords[0] == player_coords[1]
 
     def _get_next_state(self, p1, p2, actions, ball):
+        """Transition from current state to next state."""
         player_steps = ((p1 + actions[0]), (p2 + actions[1]))
         if self._check_collision(player_steps):
             self._log('collision! p1: {} p2: {}'.format(
@@ -46,6 +53,7 @@ class Transitions:
         return next_state
 
     def _handle_collision(self, p1, p2, ball, actions):
+        """Decide who moves and takes possesion of ball during collision."""
         coin_flip = np.random.randint(2)
         if self._p1_moves_first(coin_flip):
             if self._p1_has_ball(ball):
@@ -61,12 +69,15 @@ class Transitions:
         return next_state
 
     def _p1_moves_first(self, coin_flip):
+        """Check results of coinflip. If 0, P1 moves first."""
         return coin_flip == 0
 
     def _p1_has_ball(self, ball):
+        """Check if P1 has ball."""
         return ball == 0
 
     def _p1_move_ball(self, p1, ball, p2, action):
+        """P1 moves with ball."""
         p1 = p1 + action
         return self._new_state(ball, p1, p2)
 
@@ -91,6 +102,7 @@ class Transitions:
         return self._new_state(ball, p1_cell, p2_cell)
 
     def _advance(self, p1_cell, ball, p2_cell, actions):
+        """No collision detected. Both players move."""
         p1_cell = p1_cell + actions[0]
         p2_cell = p2_cell + actions[1]
         return self._new_state(ball, p1_cell, p2_cell)
